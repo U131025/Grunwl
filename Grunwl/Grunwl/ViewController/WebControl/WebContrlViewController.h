9153b2a721030d3d18fcd588c0fb0b2f403b2c24
Mojy <mojingyu82@gmail.com>
2017-08-17 16:27:37 +0800

    网络控制模块

---
+++ Grunwl/Grunwl/ViewController/WebControl/WebContrlViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebContrlViewController.h
+//  Grunwl
+//
+//  Created by Mojy on 2017/8/17.
+//  Copyright © 2017年 Mojy. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface WebContrlViewController : BaseViewController
+
+@end
---
+++ Grunwl/Grunwl/ViewController/WebControl/WebContrlViewController.m
@@ -0,0 +1,29 @@
+//
+//  WebContrlViewController.m
+//  Grunwl
+//
+//  Created by Mojy on 2017/8/17.
+//  Copyright © 2017年 Mojy. All rights reserved.
+//
+
+#import "WebContrlViewController.h"
+
+@interface WebContrlViewController ()
+
+@end
+
+@implementation WebContrlViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
